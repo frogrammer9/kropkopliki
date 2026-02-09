bedbbfc08f104f850b556746646833909fd7861c
frogrammer9 <oskar@meler.pl>
2026-02-09 13:55:01 +0100

    Configured fish

--- config/fish/abbreviations.fish
+++ config/fish/abbreviations.fish
@@ -1,3 +1,10 @@
-abbr -a v nvim
-abbr -a sv sudoedit
-abbr -a l exa
+abbr v nvim
+abbr sv sudoedit
+abbr grub-update sudo grub-mkconfig -o /boot/grub/grub.cfg
+abbr gp git push
+abbr gpl git pull
+abbr gc git commit 
+abbr gaa git add -A
+abbr ga git add
+abbr gcm git commit -m
+abbr gca git commit --amend --no-edit
---
+++ config/fish/functions/l.fish
@@ -0,0 +1,4 @@
+function l
+	clear
+	eza --icons --color=always --group-directories-first -1
+end
