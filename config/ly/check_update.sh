e461026b2d109f3f5e9b3c90e95a003634151166
frogrammer9 <oskar@meler.pl>
2026-04-18 16:25:57 +0200

    added stuff to sshu

--- config/ly/check_update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-. "$SCRIPT_DIR/../../.meta/include"
-
-if [[ "$V_MANAGE_LY" == "yes" ]] && ! cmp -s "$LYPATH/config.ini" "$SCRIPT_DIR/config.ini"; then
-	exit 0
-else
-	exit 1
-fi
--- install/ly.sh
+++ install/ly.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+DOTFILES=$(cd -- "$SCRIPT_DIR/.." && pwd)
+
 echo "Setting up ly..."
 
-sudo sed -i 's/allow_empty_password =.*/allow_empty_password = false/' /etc/ly/config.ini
-sudo sed -i 's/battery_id =.*/battery_id = 0/' /etc/ly/config.ini
-sudo sed -i 's/session_log =.*/session_log = /var/log/ly.log/' /etc/ly/config.ini
-sudo sed -i 's/save =.*/save = true/' /etc/ly/config.ini
-sudo sed -i 's/numlock =.*/numlock = true/' /etc/ly/config.ini
+sudo install -m 600 "$DOTFILES/install/ly/config.ini" /etc/ly/config.ini
 
 sudo systemctl enable --now ly@tty1.service
 sudo systemctl disable --now getty@tty1.service
