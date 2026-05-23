4d9fcee706b4e6a9cb240b670499a58c5a5e0a95
frogrammer9 <oskar@meler.pl>
2026-05-24 01:35:47 +0200

    sway config

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 DOTFILES="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-if ! [ -d "$DOTFILES/install" ]; then
-	echo "Kropkopliki have already been installed"
-	exit 127
-fi
-
 if ! sudo -v; then
 	echo "This installation script requires sudo to be installed and avaliable!"
 	exit 128
@@ -29,12 +24,11 @@ rm -rf "$HOME/.config"
 
 mkdir -p "$HOME/.local/state"
 mkdir -p "$HOME/.local/share"
-mkdir -p "$HOME/.local/logs"
 
 rm ~/.bash*
 
-ln -sf "$DOTFILES/config" "$HOME/.config"
-ln -sf "$DOTFILES/profile" "$HOME/.profile"
+cp -rf "$DOTFILES/config" "$HOME/.config"
+cp -f "$DOTFILES/profile" "$HOME/.profile"
 
 systemctl --user daemon-reload
 systemctl --user enable --now ssh-agent
@@ -42,10 +36,7 @@ systemctl --user enable --now ssh-agent
 "$DOTFILES/install/grub.sh"
 "$DOTFILES/install/plymouth.sh"
 "$DOTFILES/install/ly.sh"
-"$DOTFILES/install/browser.sh"
 "$DOTFILES/install/git.sh"
-
-rm -rf "$DOTFILES/install"
 
 read -p "Dotfiles need system restart to fully configure. Do you want to reboot now [Y/n]: " ans
 case "$ans" in 
