7c8a7726992f541556c6c67a65d739d91dc41631
frogrammer9 <oskar@meler.pl>
2026-05-24 19:01:17 +0200

    added waybar

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ===== Ensure sudo is avaliable =====
 DOTFILES="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 if ! sudo -v; then
@@ -7,9 +8,11 @@ if ! sudo -v; then
 	exit 128
 fi
 
-# install packages
+# ===== Install packages =====
 sudo pacman -Syu --needed $(< "$DOTFILES/install/pkglist.txt")
 
+# ===== User mod =====
+sudo usermod -aG adm,uucp,optical,audio,video "$USER"
 chsh -s $(whereis fish | awk '{print $2}')
 
 read -p "Do you want to back up your existing config [Y/n]: " ans
@@ -20,6 +23,7 @@ case "$ans" in
 	;;
 esac
 
+# ===== Copy config files =====
 rm -rf "$HOME/.config"
 
 mkdir -p "$HOME/.local/state"
@@ -30,14 +34,20 @@ rm ~/.bash*
 cp -rf "$DOTFILES/config" "$HOME/.config"
 cp -f "$DOTFILES/profile" "$HOME/.profile"
 
+# ===== Systemd services =====
 systemctl --user daemon-reload
 systemctl --user enable --now ssh-agent
+systemctl --user enable --now pipewire
+systemctl --user enable --now wireplumber
+systemctl --user enable --now pipewire-pulse
 
+# ===== Secondary install scripts =====
 "$DOTFILES/install/grub.sh"
 "$DOTFILES/install/plymouth.sh"
 "$DOTFILES/install/ly.sh"
 "$DOTFILES/install/git.sh"
 
+# ===== Reboot =====
 read -p "Dotfiles need system restart to fully configure. Do you want to reboot now [Y/n]: " ans
 case "$ans" in 
 	[nN]|[nN][oO]) ;;
